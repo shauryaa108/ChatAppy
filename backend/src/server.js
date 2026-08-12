4b7b16338ea74fd9de8b25d794c74a38fb91dd5b
Yash Goyal <yashgoyal.ec@gmail.com>
2026-08-13 03:45:02 +0530

    Deployment basic settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import dotenv from'dotenv'
+import path from "path"
 
 dotenv.config()
 
 const app = express();
+
+const __dirname = path.resolve();
+
+// make readt for deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))) // make frontend static assests
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 app.listen(process.env.PORT,()=>{
     console.log("App is listening")
